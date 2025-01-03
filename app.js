69bed24efbd0e1ce572880971a7c984d4a00ebd9
Oleksandr Stetsiuk <oleksandr.v.stetsiuk@gmail.com>
2025-01-03 20:22:25 +0200

    lesson03 homework

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const { read, write } = require("./fs.service");
+
 dotenv.config({ path: ".env" });
 
 const app = express();
@@ -7,71 +9,78 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-let users = [
-  {
-    id: 1,
-    name: "Oleksandr",
-    email: "super.oleksandr@email.com",
-    password: "password",
-  },
-  {
-    id: 2,
-    name: "Oleksandr",
-    email: "super.oleksandr@email.com",
-    password: "password",
-  },
-  {
-    id: 3,
-    name: "Oleksandr",
-    email: "super.oleksandr@email.com",
-    password: "password",
-  },
-  {
-    id: 4,
-    name: "Oleksandr",
-    email: "super.oleksandr@email.com",
-    password: "password",
-  },
-  {
-    id: 5,
-    name: "Oleksandr",
-    email: "super.oleksandr@email.com",
-    password: "password",
-  },
-  {
-    id: 6,
-    name: "Oleksandr",
-    email: "super.oleksandr@email.com",
-    password: "password",
-  },
-];
-
-app.get("/users", (req, res) => {
-  res.json(users);
+app.get("/users", async (_req, res) => {
+  try {
+    const users = await read();
+    res.json(users);
+  } catch (e) {
+    res.status(500).json(e.message);
+  }
 });
 
-app.post("/users", (req, res) => {
-  const user = {
-    id: users.length + 1,
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  };
-  users.push(user);
-  res.status(201).json(user);
+app.post("/users", async (req, res) => {
+  try {
+    const users = await read();
+    const user = {
+      id: users.length ? users[users.length - 1].id + 1 : 1,
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    };
+    users.push(user);
+    await write(users);
+    res.status(201).json(user);
+  } catch (e) {
+    res.status(500).json(e.message);
+  }
 });
 
-app.get("/users/:userId", (req, res) => {
+app.get("/users/:userId", async (req, res) => {
   console.log("Params: ", req.params);
   console.log("Query: ", req.query);
   console.log("Body: ", req.body);
+  const users = await read();
   const user = users.find((user) => user.id === +req.params.userId);
   res.json(user);
 });
 
-app.delete("/users/:userId", (req, res) => {
-  users = users.filter((user) => user.id != Number(req.params.userId));
-  res.json({ message: "User was deleted" });
+app.put("/users/:userId", async (req, res) => {
+  try {
+    const users = await read();
+    const index = users.findIndex(
+      (user) => user.id === Number(req.params.userId),
+    );
+    if (index === -1) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const user = users[index];
+
+    user.name = req.body.name;
+    user.email = req.body.email;
+    user.password = req.body.password;
+    write(users);
+    res.json(user);
+  } catch (e) {
+    res.status(500).json(e.message);
+  }
+});
+
+app.delete("/users/:userId", async (req, res) => {
+  try {
+    const users = await read();
+    const index = users.findIndex(
+      (user) => user.id === Number(req.params.userId),
+    );
+    if (index === -1) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    users.splice(index, 1);
+    write(users);
+    res.sendStatus(204);
+  } catch (e) {
+    res.status(500).json(e.message);
+  }
 });
 
 // create-user -> users (POST)
