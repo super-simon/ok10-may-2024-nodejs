7da79e6bb9231a576670a272362ecfd7c93d719b
Oleksandr Stetsiuk <oleksandr.v.stetsiuk@gmail.com>
2025-01-04 23:32:10 +0200

    lesson04 errors

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 
+import { ApiError } from "./errors/api-error";
 import { read, write } from "./fs.service";
 
 dotenv.config({ path: ".env" });
@@ -10,17 +11,35 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/users", async (_req: Request, res: Response): Promise<void> => {
-  try {
-    const users = await read();
-    res.json(users);
-  } catch (e) {
-    res.status(500).json(e.message);
-  }
-});
+app.get(
+  "/users",
+  async (_req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const users = await read();
+      res.json(users);
+    } catch (e) {
+      next(e);
+    }
+  },
+);
 
-app.post("/users", async (req: Request, res: Response) => {
+app.post("/users", async (req: Request, res: Response, next: NextFunction) => {
   try {
+    if (!req.body.name || req.body.name.length < 3) {
+      throw new ApiError(
+        "Name is required and should be minimum 3 symbols",
+        400,
+      );
+    }
+    if (!req.body.email || !req.body.email.includes("@")) {
+      throw new ApiError("Email is reqired.", 400);
+    }
+    if (!req.body.password || req.body.password.length < 8) {
+      throw new ApiError(
+        "Password is required and should be minimum 8 symbols",
+        400,
+      );
+    }
     const users = await read();
     const user = {
       id: users.length ? users[users.length - 1].id + 1 : 1,
@@ -32,60 +51,105 @@ app.post("/users", async (req: Request, res: Response) => {
     await write(users);
     res.status(201).json(user);
   } catch (e) {
-    res.status(500).json(e.message);
+    next(e);
   }
 });
 
-app.get("/users/:userId", async (req: Request, res: Response) => {
-  console.log("Params: ", req.params);
-  console.log("Query: ", req.query);
-  console.log("Body: ", req.body);
-  const users = await read();
-  const user = users.find((user) => user.id === +req.params.userId);
-  res.json(user);
-});
-
-app.put("/users/:userId", async (req: Request, res: Response): Promise<any> => {
-  try {
-    const users = await read();
-    const index = users.findIndex(
-      (user) => user.id === Number(req.params.userId),
-    );
-    if (index === -1) {
-      return res.status(404).json({ message: "User not found" });
+app.get(
+  "/users/:userId",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      console.log("Params: ", req.params);
+      console.log("Query: ", req.query);
+      console.log("Body: ", req.body);
+      const users = await read();
+      const user = users.find((user) => user.id === +req.params.userId);
+      if (!user) {
+        throw new ApiError("User not found", 404);
+      }
+      res.json(user);
+    } catch (e) {
+      next(e);
     }
+  },
+);
 
-    const user = users[index];
+app.put(
+  "/users/:userId",
+  async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+    try {
+      if (!req.body.name || req.body.length < 3) {
+        throw new ApiError(
+          "Name is required and should be minimum 3 symbols",
+          400,
+        );
+      }
+      if (!req.body.email || !req.body.email.includes("@")) {
+        throw new ApiError("Email is reqired.", 400);
+      }
+      if (!req.body.password || req.body.password.length < 8) {
+        throw new ApiError(
+          "Password is required and should be minimum 8 symbols",
+          400,
+        );
+      }
+      const users = await read();
+      const index = users.findIndex(
+        (user) => user.id === Number(req.params.userId),
+      );
 
-    user.name = req.body.name;
-    user.email = req.body.email;
-    user.password = req.body.password;
-    write(users);
-    res.json(user);
-  } catch (e) {
-    res.status(500).json(e.message);
-  }
-});
+      if (index === -1) {
+        throw new ApiError("User not found", 404);
+      }
+
+      const user = users[index];
+
+      user.name = req.body.name;
+      user.email = req.body.email;
+      user.password = req.body.password;
+      write(users);
+      res.json(user);
+    } catch (e) {
+      next(e);
+    }
+  },
+);
 
 app.delete(
   "/users/:userId",
-  async (req: Request, res: Response): Promise<any> => {
+  async (req: Request, res: Response, next: NextFunction): Promise<any> => {
     try {
       const users = await read();
       const index = users.findIndex(
         (user) => user.id === Number(req.params.userId),
       );
+
       if (index === -1) {
-        return res.status(404).json({ message: "User not found" });
+        throw new ApiError("User not found", 404);
       }
+
       users.splice(index, 1);
       write(users);
       res.sendStatus(204);
     } catch (e) {
-      res.status(500).json(e.message);
+      next(e);
     }
   },
 );
+
+app.use(
+  "*",
+  (error: ApiError, _req: Request, res: Response, _next: NextFunction) => {
+    const status = error.status ?? 500;
+    const message = error.message ?? "Something went wrong.";
+    res.status(status).json({ status, message });
+  },
+);
+
+process.on("uncaughtException", (error) => {
+  console.log("Uncaught Exception: ", error);
+  process.exit(1);
+});
 
 // create-user -> users (POST)
 // get-list-users -> users (GET)
