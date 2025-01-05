eb741a71d6ed5e658982d6bf715b41a7e109f9e7
Oleksandr Stetsiuk <oleksandr.v.stetsiuk@gmail.com>
2025-01-05 23:47:28 +0200

    lesson04 consultation. Validation

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,14 +1,40 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 
 import { userController } from "../controllers/user.controller";
+import { ApiError } from "../errors/api-error";
 
 const router = Router();
 
+const validateUserData = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const dto = req.body;
+    if (!dto.name || dto.name.length < 3) {
+      throw new ApiError(
+        "Name is required and should be minimum 3 symbols",
+        400,
+      );
+    }
+    if (!dto.email || !dto.email.includes("@")) {
+      throw new ApiError("Email is required", 400);
+    }
+    if (!dto.password || dto.password.length < 8) {
+      throw new ApiError(
+        "Password is required and should be minimum 8 symbols",
+        400,
+      );
+    }
+    req.body = dto;
+    next();
+  } catch (e) {
+    next(e);
+  }
+};
+
 router.get("/", userController.getList);
-router.post("/", userController.create);
+router.post("/", validateUserData, userController.create);
 
 router.get("/:userId", userController.getUserById);
-router.put("/:userId", userController.updateUser);
+router.put("/:userId", validateUserData, userController.updateUser);
 router.delete("/:userId", userController.deleteUser);
 
 export const userRouter = router;
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -8,22 +8,6 @@ class UserService {
   }
 
   public async create(dto: IUserDto): Promise<IUser> {
-    if (!dto.name || dto.name.length < 3) {
-      throw new ApiError(
-        "Name is required and should be minimum 3 symbols",
-        400,
-      );
-    }
-    if (!dto.email || !dto.email.includes("@")) {
-      throw new ApiError("Email is required", 400);
-    }
-    if (!dto.password || dto.password.length < 8) {
-      throw new ApiError(
-        "Password is required and should be minimum 8 symbols",
-        400,
-      );
-    }
-
     return await userRepository.create(dto);
   }
 
@@ -36,22 +20,6 @@ class UserService {
   }
 
   public async updateUser(userId: number, dto: IUserDto): Promise<IUser> {
-    if (!dto.name || dto.name.length < 3) {
-      throw new ApiError(
-        "Name is required and should be minimum 3 symbols",
-        400,
-      );
-    }
-    if (!dto.email || !dto.email.includes("@")) {
-      throw new ApiError("Email is required", 400);
-    }
-    if (!dto.password || dto.password.length < 8) {
-      throw new ApiError(
-        "Password is required and should be minimum 8 symbols",
-        400,
-      );
-    }
-
     const user = await userRepository.getById(userId);
     if (!user) {
       throw new ApiError("User not found", 404);
